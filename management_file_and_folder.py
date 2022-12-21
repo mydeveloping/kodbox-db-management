369c3ebd2d9490a649f0d92447e1e63e827e13eb
mydeveloping <abupnc@gmail.com>
2022-12-22 01:31:19 +0330

    Ignore duplicate names
    Ignore duplicate names for both of files and folders
    add create file/folder with parent id

--- management_file_and_folder.py
+++ management_file_and_folder.py
@@ -1,12 +1,20 @@
+from os import name
 from random import randint as ri
 import db_tools as db
 import re
+from datetime import datetime
 
 # Add relation between files or folders
 class Relation_Utility:
-    def __related_ids(self, book_name):
+
+    def __now_timestamp(self):
+        # datetime object containing current date and time
+        now = datetime.now()
+        return str(int(now.timestamp()))
+
+    def __related_ids(self, relations_string):
         regex = r"{source:[\d]+}"
-        matches = re.finditer(regex, book_name, re.MULTILINE)
+        matches = re.finditer(regex, relations_string, re.MULTILINE)
         found_tags = []
         for matchNum, match in enumerate(matches, start=1):
             g = match.group().replace('(', '').replace(')', '')
@@ -33,10 +41,9 @@ class Relation_Utility:
         if source_id == related_id:
             raise Exception("Error: source and relation must be different.\nYour source and related id is \"" + str(source_id) + "\"")
         relations = self.__get_relations(source_id)
-        print('relations ', relations)
         if relations[0] == 0 and len(relations) == 1:
             query = 'INSERT INTO `io_source_meta` (`sourceID`, `key`, `value`, `createTime`, `modifyTime`) VALUES\
-                (' + str(source_id) + ', \'user_sourceAlias\', \'["\{source:' + str(related_id) + '\}/"]\', 1669968606, 1669968606)'
+                (' + str(source_id) + ', \'user_sourceAlias\', \'["\{source:' + str(related_id) + '\}/"]\', ' + str(self.__now_timestamp()) + ', ' + str(self.__now_timestamp()) + ')'
             ins = db._insert(query)
             if ins[0] == -1:
                 raise Exception("Error on insert to db (\"add_relation\")\n", ins[1])
@@ -61,8 +68,14 @@ class Relation_Utility:
         else:
             raise Exception("Error on select from db (\"add_relation\")\n")
 
-# Create file of folder in kodbox            
+# Create file or folder in kodbox            
 class File_and_Folder_Utility:
+
+    def __now_timestamp(self):
+        # datetime object containing current date and time
+        now = datetime.now()
+        return str(int(now.timestamp()))
+
     def __create_random_list(self, min, max, length):
         randomlist = []
         for i in range(0,length):
@@ -99,8 +112,8 @@ class File_and_Folder_Utility:
             is_folder = 1
         else:
             is_folder = 0
-        query = """select `sourceID` from `io_source` where `isFolder` = {} and `name` = '{}'
-            order by `sourceID` limit 1""".format(is_folder, name)
+        query = """select `sourceID` from `io_source` where `name` = '{}' and `isFolder` = {}
+            order by `sourceID` limit 1""".format(name, is_folder)
         sel = db._select(query)
         if sel[0] == -1:
             raise Exception("Error on select from db (\"__get_id\")\n", sel[1])
@@ -110,7 +123,58 @@ class File_and_Folder_Utility:
             else:
                 return sel[2][0][0]
 
+    def __get_parent_id(self, name_id, is_folder = 1):
+        if (is_folder == True) or (is_folder == 1):
+            is_folder = 1
+        else:
+            is_folder = 0
+        query = """select parentID from io_source where sourceID = {} and isFolder = {}""".format(name_id, is_folder)
+        sel = db._select(query)
+        if sel[0] == -1:
+            raise Exception("Error on select from db (\"__get_parent_id\")\n", sel[1])
+        else:
+            if sel[1] == 0:
+                return -1
+            else:
+                return sel[2][0][0]
+
+    def __get_name_id(self, name, parent_name, is_folder = 1):
+        if (is_folder == True) or (is_folder == 1):
+            is_folder = 1
+        else:
+            is_folder = 0
+        query = """select io_source.sourceID from io_source, io_source as parent_source
+            where `io_source`.`name` = '{name}' and io_source.parentID = parent_source.sourceID 
+            and parent_source.`name` = '{parent_name}'
+            and io_source.isFolder = {is_folder}""".format(name=name, parent_name=parent_name, is_folder=is_folder)
+        sel = db._select(query)
+        if sel[0] == -1:
+            raise Exception("Error on select from db (\"__get_name_id\")\n", sel[1])
+        else:
+            if sel[1] == 0:
+                return -1
+            else:
+                return sel[2][0][0]
+
+    def __get_name_id_with_parent_id(self, name, parent_id, is_folder = 1):
+        if (is_folder == True) or (is_folder == 1):
+            is_folder = 1
+        else:
+            is_folder = 0
+        query = """select sourceID from io_source where `name` = '{}' and parentID = {} and isFolder = {}""".format(name, parent_id, is_folder)
+        sel = db._select(query)
+        if sel[0] == -1:
+            raise Exception("Error on select from db (\"__get_name_id_with_parent_id\")\n", sel[1])
+        else:
+            if sel[1] == 0:
+                return -1
+            else:
+                return sel[2][0][0]
+
     def __create_folder(self, folder_name, parent_name):
+        folder_id = self.__get_name_id(folder_name, parent_name, 1)
+        if folder_id != -1:
+            return folder_id
         parent_id = self.__get_id(parent_name, 1)
         if (parent_id <= 0):
             self.__create_folder(parent_name, 'Desktop')
@@ -119,12 +183,33 @@ class File_and_Folder_Utility:
             (`sourceHash`, `targetType`, `targetID`, `createUser`, `modifyUser`, `isFolder`, `name`, `fileType`,
                 `parentID`, `parentLevel`, `fileID`, `isDelete`, `size`, `createTime`, `modifyTime`, `viewTime`)
             VALUES ('{}', 1, 1, 1, 1, 1, '{}', '', {}, 
-            '{}{},',0, 0, 0, 1669968888, 1669968918, 1669968888)'''.format(
+            '{}{},',0, 0, 0, {}, {}, {})'''.format(
                 self.__calc_hash(), folder_name, parent_id, 
-                self.__get_level(parent_id), self.__get_id(parent_name, 1))
+                self.__get_level(parent_id), self.__get_id(parent_name, 1),
+                str(self.__now_timestamp()), str(self.__now_timestamp()), str(self.__now_timestamp()))
         ins = db._insert(query)
         if ins[0] == -1:
             raise Exception("Error on insert to db (\"__create_folder\")\n", ins[1])
+        else:
+            return ins[1]
+
+    def __create_folder_with_parent_id(self, folder_name, parent_id):
+        folder_id = self.__get_name_id_with_parent_id(folder_name, parent_id, 1)
+        if folder_id != -1:
+            return folder_id
+        if (parent_id <= 0):
+            raise Exception("Error on select parent folder (\"__create_folder_with_parent_id\")\n")
+        query = '''INSERT INTO `io_source` 
+            (`sourceHash`, `targetType`, `targetID`, `createUser`, `modifyUser`, `isFolder`, `name`, `fileType`,
+                `parentID`, `parentLevel`, `fileID`, `isDelete`, `size`, `createTime`, `modifyTime`, `viewTime`)
+            VALUES ('{}', 1, 1, 1, 1, 1, '{}', '', {}, 
+            '{}{},',0, 0, 0, {}, {}, {})'''.format(
+                self.__calc_hash(), folder_name, parent_id, 
+                self.__get_level(parent_id), parent_id,
+                str(self.__now_timestamp()), str(self.__now_timestamp()), str(self.__now_timestamp()))
+        ins = db._insert(query)
+        if ins[0] == -1:
+            raise Exception("Error on insert to db (\"__create_folder_with_parent_id\")\n", ins[1])
         else:
             return ins[1]
 
@@ -136,6 +221,7 @@ class File_and_Folder_Utility:
         else:
             is_folder = 0
             create_type = 'mkfile'
+        name = name.replace("''", "'")
         query = 'select `sourceID`, `parentID` from `io_source` where `name` = "{}" and `isFolder` = {} order by `sourceID` limit 1'.format(name, is_folder)
         sel = db._select(query)
         if sel[0] == -1:
@@ -145,9 +231,19 @@ class File_and_Folder_Utility:
                 self.__create_folder(name, 'Desktop')
             else:
                 sourceID, parentID = sel[2][0][0], sel[2][0][1]
+        name = name.replace("'", "''")
+        query_select = 'SELECT id FROM `io_source_event` where sourceID = ' \
+            + str(sourceID) + ' and `sourceParent` = ' + str(parentID) + ' '\
+        ' and `desc` = \'{\n    "createType": "' + create_type + '",\n    "name": "' + name + '"\n}\''
+        sel = db._select(query_select)
+        if sel[0] == -1:
+            raise Exception('Error on select db\n', sel[1])
+        elif sel[1] != 0:
+            return sel[2][0][0]
+
         query = 'INSERT INTO `io_source_event` (`sourceID`, `sourceParent`, `userID`, `type`, `desc`, `createTime`) VALUES'\
         '(' + str(sourceID) + ', ' + str(parentID) + ' '\
-        ', 1, \'create\', \'{\n    "createType": "'+ create_type +'",\n    "name": "' + name + '"\n}\', 1669968888)'
+        ', 1, \'create\', \'{\n    "createType": "'+ create_type +'",\n    "name": "' + name + '"\n}\', ' + str(self.__now_timestamp()) + ')'
         ins = db._insert(query)
         if ins[0] == -1:
             raise Exception("Error on insert to db (\"__create_source_event\")\n", ins[1])
@@ -174,7 +270,7 @@ class File_and_Folder_Utility:
         '(\'' + str(session_id) + '\', 1, \'file.' + type + '\', \'{"sourceID":' + str(source_id) + ',"sourceParent":"'\
         ' ' + str(source_parent_id) + '","pathName":"' + name + '","pathDisplay":"","userID":"1",'\
         '"type":"create","desc":{"createType":"' + type + '","name":"' + name + '"},'\
-        '"sourceTarget":' + str(source_id) + ',"ip":"127.0.0.1"}\', 1669968888)'
+        '"sourceTarget":' + str(source_id) + ',"ip":"127.0.0.1"}\', ' + str(self.__now_timestamp()) + ')'
         ins = db._insert(query)
         if ins[0] == -1:
             raise Exception("Error on insert to db (\"__create_system_log\")\n", ins[1])
@@ -193,19 +289,44 @@ class File_and_Folder_Utility:
                 return sel[2][0][0]
 
     def __create_file(self, file_name, parent_name):
+        file_id = self.__get_name_id(file_name, parent_name, 0)
+        if file_id != -1:
+            return file_id
         parent_id = self.__get_id(parent_name, 1)
         if (parent_id <= 0):
-            self.__create_folder(parent_name, 'Desktop')
-        parent_id = self.__get_id(parent_name, 1)
+            parent_id = self.__create_folder(parent_name, 'Desktop')
         file_type = file_name.split('.')
         file_type = file_type[len(file_type) - 1]
         query = '''INSERT INTO `io_source` 
             (`sourceHash`, `targetType`, `targetID`, `createUser`, `modifyUser`, `isFolder`, `name`, `fileType`,
                 `parentID`, `parentLevel`, `fileID`, `isDelete`, `size`, `createTime`, `modifyTime`, `viewTime`)
             VALUES ('{}', 1, 1, 1, 1, 0, '{}', '{}', {}, 
-            '{}{},',{}, 0, 0, 1669968888, 1669968918, 1669968888)'''.format(
+            '{}{},',{}, 0, 0, {}, {}, {})'''.format(
                 self.__calc_hash(), file_name, file_type, parent_id, 
-                self.__get_level(parent_id), self.__get_id(parent_name, 1), self.__count_parent_id(parent_id))
+                self.__get_level(parent_id), parent_id, self.__count_parent_id(parent_id),
+                str(self.__now_timestamp()), str(self.__now_timestamp()), str(self.__now_timestamp()))
+        ins = db._insert(query)
+        if ins[0] == -1:
+            raise Exception("Error on insert to db (\"__create_file\")\n", ins[1])
+        else:
+            return ins[1]
+
+    def __create_file_with_parent_id(self, file_name, parent_id):
+        file_id = self.__get_name_id_with_parent_id(file_name, parent_id, 0)
+        if file_id != -1:
+            return file_id
+        if (parent_id <= 0):
+            return -1
+        file_type = file_name.split('.')
+        file_type = file_type[len(file_type) - 1]
+        query = '''INSERT INTO `io_source` 
+            (`sourceHash`, `targetType`, `targetID`, `createUser`, `modifyUser`, `isFolder`, `name`, `fileType`,
+                `parentID`, `parentLevel`, `fileID`, `isDelete`, `size`, `createTime`, `modifyTime`, `viewTime`)
+            VALUES ('{}', 1, 1, 1, 1, 0, '{}', '{}', {}, 
+            '{}{},',{}, 0, 0, {}, {}, {})'''.format(
+                self.__calc_hash(), file_name, file_type, parent_id, 
+                self.__get_level(parent_id), parent_id, self.__count_parent_id(parent_id),
+                str(self.__now_timestamp()), str(self.__now_timestamp()), str(self.__now_timestamp()))
         ins = db._insert(query)
         if ins[0] == -1:
             raise Exception("Error on insert to db (\"__create_file\")\n", ins[1])
@@ -214,8 +335,23 @@ class File_and_Folder_Utility:
 
     def create_folder_on_kodbox(self, folder_name, parent_name, session_id):
         try:
+            folder_name = str(folder_name)
+            parent_name = str(parent_name)
+            folder_name = folder_name.replace("'", "''")
+            parent_name = parent_name.replace("'", "''")
             folder_id = self.__create_folder(folder_name, parent_name)
-            parent_id = self.__get_id(parent_name, 1)
+            parent_id = self.__get_parent_id(folder_id, 1)
+            self.__create_source_event(folder_name, 1)
+            self.__create_system_log(session_id, folder_id, parent_id, folder_name, 1)
+            return (folder_id, parent_id)
+        except Exception as e:
+            raise Exception('When create folder below error raised:\n', e)
+
+    def create_folder_on_kodbox_with_parent_id(self, folder_name, parent_id, session_id):
+        try:
+            folder_name = str(folder_name)
+            folder_name = folder_name.replace("'", "''")
+            folder_id = self.__create_folder_with_parent_id(folder_name, parent_id)
             self.__create_source_event(folder_name, 1)
             self.__create_system_log(session_id, folder_id, parent_id, folder_name, 1)
             return (folder_id, parent_id)
@@ -224,8 +360,23 @@ class File_and_Folder_Utility:
 
     def create_file_on_kodbox(self, file_name, parent_name, session_id):
         try:
+            file_name = str(file_name)
+            parent_name = str(parent_name)
+            file_name = file_name.replace("'", "''")
+            parent_name = parent_name.replace("'", "''")
             file_id = self.__create_file(file_name, parent_name)
-            parent_id = self.__get_id(parent_name, 1)
+            parent_id = self.__get_parent_id(file_id, 0)
+            self.__create_source_event(file_name, 0)
+            self.__create_system_log(session_id, file_id, parent_id, file_name, 1)
+            return (file_id, parent_id)
+        except Exception as e:
+            raise Exception('When create file below error raised:\n', e) 
+
+    def create_file_on_kodbox_with_parent_id(self, file_name, parent_id, session_id):
+        try:
+            file_name = str(file_name)
+            file_name = file_name.replace("'", "''")
+            file_id = self.__create_file_with_parent_id(file_name, parent_id)
             self.__create_source_event(file_name, 0)
             self.__create_system_log(session_id, file_id, parent_id, file_name, 1)
             return (file_id, parent_id)
